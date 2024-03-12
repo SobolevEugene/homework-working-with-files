f1bd285a38044b785786a23dde2f2e974f501cb0
SobolevEA <SobolevEA@dgtserv.ru>
2024-03-12 11:48:43 +0300

    2. Документы водителя

--- src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
+++ src/main/java/com/sample/drivers/screen/driver/DriverEdit.java
@@ -1,7 +1,12 @@
 package com.sample.drivers.screen.driver;
 
+import com.sample.drivers.entity.Document;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.Component;
 import io.jmix.ui.component.Image;
+import io.jmix.ui.component.LinkButton;
 import io.jmix.ui.component.ResourceView;
+import io.jmix.ui.download.Downloader;
 import io.jmix.ui.screen.*;
 import com.sample.drivers.entity.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +17,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class DriverEdit extends StandardEditor<Driver> {
 	@Autowired
 	private Image<byte[]> photoField;
+	@Autowired
+	private UiComponents uiComponents;
+	@Autowired
+	private Downloader downloader;
 
 	@Subscribe
 	public void onBeforeShow(final BeforeShowEvent event) {
 		checkPhotoVisibility();
 	}
-
 
 
 	@Subscribe("photoField")
@@ -27,6 +35,18 @@ public class DriverEdit extends StandardEditor<Driver> {
 
 	private void checkPhotoVisibility() {
 		photoField.setVisible(getEditedEntity().getPhoto() != null);
+	}
+
+	@Install(to = "documentsTable.file", subject = "columnGenerator")
+	private Component documentsTableFileColumnGenerator(final Document document) {
+		if (document.getFile() == null) {
+			return null;
+		}
+		var saveButton = uiComponents.create(LinkButton.class);
+		saveButton.setCaption(document.getFile().getFileName());
+		saveButton.addClickListener(clickEvent ->
+				downloader.download(document.getFile()));
+		return saveButton;
 	}
 
 
